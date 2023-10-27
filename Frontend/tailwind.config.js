3881b8a3403f105745cf7e6e9c38a71c7ca25c2a
mctuliomontoya <mctulio.2@gmail.com>
2023-10-26 18:01:13 -0700

    feat: add orange primary color

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -2,10 +2,13 @@
 module.exports = {
   content: ["./App.{js,jsx,ts,tsx}", "./screens/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
-    colors: {
-      'orange' : "#E69436", 
+    extend: {
+      colors: {
+        'orange' : "#FF8000", 
+        
+      },
     },
+    
   },
   plugins: [],
 }
