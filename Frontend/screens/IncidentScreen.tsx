fb1b50ace8545917a20fa15348ea12687a1a579e
mctuliomontoya <mctulio.2@gmail.com>
2023-11-01 18:55:09 -0700

    feat: add multiple container compatibility

--- Frontend/screens/IncidentScreen.tsx
+++ Frontend/screens/IncidentScreen.tsx
@@ -4,28 +4,29 @@ import EditScreenInfo from "../components/EditScreenInfo";
 import { Text, View } from "../components/Themed";
 import Container from "../components/Container";
 
-import { styled } from 'nativewind';
+import { styled } from "nativewind";
 
 import Background from "../components/Background";
 
-const StyledView = styled(View)
-const StyledText = styled(Text)
+const StyledView = styled(View);
+const StyledText = styled(Text);
 
 export default function IncidentScreen() {
   return (
     <StyledView className="flex-1">
       <Background>
-              <Container>
-        <StyledText className="text-black  text-xl font-bold">Incidentes</StyledText>
-        <View
-          style={styles.separator}
-          lightColor="#eee"
-          darkColor="rgba(255,255,255,0.1)"
-        />
-        <EditScreenInfo path="/screens/TabOneScreen.tsx" />
-      </Container>
+        <Container>
+          <StyledText className="text-black  text-xl font-bold">
+            Incidentes
+          </StyledText>
+          <View
+            style={styles.separator}
+            lightColor="#eee"
+            darkColor="rgba(255,255,255,0.1)"
+          />
+          <EditScreenInfo path="/screens/TabOneScreen.tsx" />
+        </Container>
       </Background>
-
     </StyledView>
   );
 }
