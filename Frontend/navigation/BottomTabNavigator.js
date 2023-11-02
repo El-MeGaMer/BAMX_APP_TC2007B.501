262123bbcc415853167792f451152aece347d398
mctuliomontoya <mctulio.2@gmail.com>
2023-11-01 17:19:05 -0700

    change top bar styles

--- Frontend/navigation/BottomTabNavigator.js
+++ Frontend/navigation/BottomTabNavigator.js
@@ -1,7 +1,6 @@
 // Learn more about createBottomTabNavigator:
 // https://reactnavigation.org/docs/bottom-tab-navigator
-import { MaterialCommunityIcons } from '@expo/vector-icons'; 
-
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { createStackNavigator } from "@react-navigation/stack";
@@ -28,6 +27,7 @@ export default function BottomTabNavigator() {
         name="Home"
         component={HomeNavigator}
         options={{
+          cardSyle: {},
           headerShown: false,
           tabBarIcon: ({ color }) => (
             <TabBarIcon name="home-outline" color={color} />
@@ -35,42 +35,32 @@ export default function BottomTabNavigator() {
         }}
       />
       <BottomTab.Screen
-        name="Calendar"
-        component={CalendarNavigator}
-        options={{
-          headerShown: false,
-          tabBarIcon: ({ color }) => (
-            <TabBarIcon name="calendar-month-outline" color={color} />
-          ),
-        }}
-      />
-      <BottomTab.Screen 
         name="Incident"
         component={IncidentNavigator}
         options={{
           headerShown: false,
           tabBarIcon: ({ color }) => (
-            <TabBarIcon name ="alert-octagon-outline" color={ color } />
+            <TabBarIcon name="alert-octagon-outline" color={color} />
           ),
         }}
       />
-      <BottomTab.Screen 
+      <BottomTab.Screen
         name="Logs"
         component={LogsNavigator}
         options={{
           headerShown: false,
-          tabBarIcon: ({color}) => (
-            <TabBarIcon name="format-list-bulleted-type" color={ color}/>
+          tabBarIcon: ({ color }) => (
+            <TabBarIcon name="format-list-bulleted-type" color={color} />
           ),
         }}
       />
-      <BottomTab.Screen 
+      <BottomTab.Screen
         name="User"
         component={UserNavigator}
         options={{
           headerShown: false,
-          tabBarIcon: ({color}) => (
-            <TabBarIcon name="account-circle-outline" color={ color}/>
+          tabBarIcon: ({ color }) => (
+            <TabBarIcon name="account-circle-outline" color={color} />
           ),
         }}
       />
@@ -81,7 +71,9 @@ export default function BottomTabNavigator() {
 // You can explore the built-in icon families and icons on the web at:
 // https://icons.expo.fyi/
 function TabBarIcon(props) {
-  return <MaterialCommunityIcons size={30} style={{ marginBottom: -3 }} {...props} />;
+  return (
+    <MaterialCommunityIcons size={30} style={{ marginBottom: -3 }} {...props} />
+  );
 }
 
 // Each tab has its own navigation stack, you can read more about this pattern here:
@@ -94,64 +86,99 @@ function HomeNavigator() {
       <HomeStack.Screen
         name="HomeScreen"
         component={HomeScreen}
-        options={{headerTitle : "Home Title"}}
+        options={{
+          headerTitle: "Home Title",
+          headerStyle: {
+            backgroundColor: "#FF8000",
+            elevation: 0,
+            shadowOpacity: 0,
+            borderBottomWidth: 0,
+          },
+          headerTitleStyle: {
+            fontWeight: "bold",
+            color: "white",
+          },
+        }}
       />
     </HomeStack.Navigator>
-  )
+  );
 }
 
-const CalendarStack = createStackNavigator();
-
-function CalendarNavigator() {
-  return (
-    <CalendarStack.Navigator>
-      <CalendarStack.Screen 
-        name = "CalendarScreen"
-        component = {CalendarScreen}
-        options = {{headerTitle : "Calendario"}}
-      />
-    </CalendarStack.Navigator>
-  )
-}
 
 const IncidentStack = createStackNavigator();
 
-function IncidentNavigator () {
+function IncidentNavigator() {
   return (
     <IncidentStack.Navigator>
-      <IncidentStack.Screen 
-        name = "IncidentScreen"
-        component = {IncidentScreen}
-        options = {{headerTitle : "Incidentes"}}
+      <IncidentStack.Screen
+        name="IncidentScreen"
+        component={IncidentScreen}
+        options={{
+          headerTitle: "Incidentes",
+          headerStyle: {
+            backgroundColor: "#FF8000",
+            elevation: 0,
+            shadowOpacity: 0,
+            borderBottomWidth: 0,
+          },
+          headerTitleStyle: {
+            fontWeight: "bold",
+            color: "white",
+          },
+        }}
       />
     </IncidentStack.Navigator>
-  )
+  );
 }
 
 const LogsStack = createStackNavigator();
 
-function LogsNavigator () {
+function LogsNavigator() {
   return (
     <LogsStack.Navigator>
-      <LogsStack.Screen 
-        name = "LogsScreen"
-        component = {LogsScreen}
-        options = {{headerTitle : "Bitácoras"}}
+      <LogsStack.Screen
+        name="LogsScreen"
+        component={LogsScreen}
+        options={{
+          headerTitle: "Bitácoras",
+          headerStyle: {
+            backgroundColor: "#FF8000",
+            elevation: 0,
+            shadowOpacity: 0,
+            borderBottomWidth: 0,
+          },
+          headerTitleStyle: {
+            fontWeight: "bold",
+            color: "white",
+          },
+        }}
       />
     </LogsStack.Navigator>
-  )
+  );
 }
 
 const UserStack = createStackNavigator();
 
-function UserNavigator () {
+function UserNavigator() {
   return (
     <UserStack.Navigator>
-      <UserStack.Screen 
-        name = "UserScreen"
-        component = {UserScreen}
-        options = {{headerTitle : "Usuario"}}
+      <UserStack.Screen
+        name="UserScreen"
+        component={UserScreen}
+        options={{
+          headerTitle: "Usuario",
+          headerStyle: {
+            backgroundColor: "#FF8000",
+            elevation: 0,
+            shadowOpacity: 0,
+            borderBottomWidth: 0,
+          },
+          headerTitleStyle: {
+            fontWeight: "bold",
+            color: "white",
+          },
+        }}
       />
     </UserStack.Navigator>
-  )
+  );
 }
