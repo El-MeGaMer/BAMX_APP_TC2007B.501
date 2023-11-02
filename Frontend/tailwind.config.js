f428b69d7b51cb577fed808ccfd553bf34531a39
mctuliomontoya <mctulio.2@gmail.com>
2023-11-01 17:17:53 -0700

    add: add new styles and colors

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -1,12 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./App.{js,jsx,ts,tsx}", "./screens/**/*.{js,jsx,ts,tsx}"],
+  mode: "jit",
+  content: ["./App.{js,jsx,ts,tsx}", "./screens/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
+  darkMode: "media", // or 'media' or 'class
   theme: {
     extend: {
       colors: {
         'orange' : "#FF8000", 
+        "gray-1": "#A0A0A0",
+        'red-hightlight': "#FF0025",
         
       },
+      fontFamily: {
+        interThin: ['Inter_100Thin'],
+        Inter_900Black: ['Inter_900Black'],
+        
+      }
     },
     
   },
