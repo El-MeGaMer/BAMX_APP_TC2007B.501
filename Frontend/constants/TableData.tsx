090874cc5a37361ab7a3689ce013638d01b23009
mctuliomontoya <mctulio.2@gmail.com>
2023-11-08 18:18:15 -0700

    create data constants example

---
+++ Frontend/constants/TableData.tsx
@@ -0,0 +1,25 @@
+import { Text, View , TextInput} from 'react-native'
+import React, { Component } from 'react'
+import { CheckBox } from "react-native-elements";
+
+export const TableData = {
+    bitacora_limpieza: [
+      ["Campo 1", <CheckBox />],
+      ["Campo 2", <TextInput />],
+      ["Campo 3", <CheckBox />],
+    ],
+
+    bitacora_x: [
+      ["Baños", <CheckBox />],
+      ["TableB-Row 2, Cell 1", <TextInput />],
+      ["TableB-Row 3, Cell 1", <CheckBox />],
+    ],
+
+    bitacora_extintores: [
+      ["Cell 1", <CheckBox />],
+      ["Cell 2", <TextInput />],
+      ["Cell 3", <CheckBox />],
+    ],
+  };
+
+  
