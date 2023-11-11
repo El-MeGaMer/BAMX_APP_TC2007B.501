c3486fc182f2dd575476fffd4c0e5123d451418c
Dany <daniela.ruizgar1@gmail.com>
2023-11-10 18:17:43 -0700

    feat: added calendar and schedule for home screen

--- Frontend/screens/HomeScreen.tsx
+++ Frontend/screens/HomeScreen.tsx
@@ -1,36 +1,187 @@
-import { StyleSheet } from "react-native";
+import React, { useState, Component } from "react";
+import { StyleSheet, Alert, Text, View, TouchableOpacity } from "react-native";
 
 import EditScreenInfo from "../components/EditScreenInfo";
-import { Text, View } from "../components/Themed";
+//import { Text, View } from "../components/Themed";
 import Container from "../components/Container";
 import Background from "../components/Background";
+import {
+  Calendar,
+  CalendarList,
+  Agenda,
+  LocaleConfig,
+  DateData,
+  AgendaEntry,
+  AgendaSchedule,
+} from "react-native-calendars";
 
-import { styled } from "nativewind";
+import testIDs from "../test/testIDs";
 
-const StyledView = styled(View);
+//import { styled } from "nativewind";
 
-export default function HomeScreen() {
+// const StyledView = styled(View);
+
+/* export default function HomeScreen() {
+  const [selected, setSelected] = useState("");
   return (
     <StyledView className="flex-1">
-      <Background>
-        <Container>
-          <Text style={styles.title}>Home Page</Text>
-          <View
-            style={styles.separator}
-            lightColor="#eee"
-            darkColor="rgba(255,255,255,0.1)"
-          />
-          <EditScreenInfo path="/screens/TabOneScreen.tsx" />
-          <EditScreenInfo path="/screens/TabOneScreen.tsx" />
-          <EditScreenInfo path="/screens/TabOneScreen.tsx" />
-          <EditScreenInfo path="/screens/TabOneScreen.tsx" />
-        </Container>
-      </Background>
+      <Calendar
+        onDayPress={(day) => {
+          setSelected(day.dateString);
+        }}
+        markedDates={{
+          [selected]: {
+            selected: true,
+            disableTouchEvent: true,
+            selectedColor: "orange",
+          },
+        }}
+      />
     </StyledView>
   );
+} */
+
+interface State {
+  items?: AgendaSchedule;
+}
+
+export default class AgendaScreen extends Component<State> {
+  state: State = {
+    items: undefined,
+  };
+
+  // reservationsKeyExtractor = (item, index) => {
+  //   return `${item?.reservation?.day}${index}`;
+  // };
+
+  render() {
+    return (
+      <Agenda
+        testID={testIDs.agenda.CONTAINER}
+        items={this.state.items}
+        loadItemsForMonth={this.loadItems}
+        selected={"2023-11-10"}
+        renderItem={this.renderItem}
+        renderEmptyDate={this.renderEmptyDate}
+        rowHasChanged={this.rowHasChanged}
+        showClosingKnob={true}
+        // markingType={'period'}
+        // markedDates={{
+        //    '2017-05-08': {textColor: '#43515c'},
+        //    '2017-05-09': {textColor: '#43515c'},
+        //    '2017-05-14': {startingDay: true, endingDay: true, color: 'blue'},
+        //    '2017-05-21': {startingDay: true, color: 'blue'},
+        //    '2017-05-22': {endingDay: true, color: 'gray'},
+        //    '2017-05-24': {startingDay: true, color: 'gray'},
+        //    '2017-05-25': {color: 'gray'},
+        //    '2017-05-26': {endingDay: true, color: 'gray'}}}
+        // monthFormat={'yyyy'}
+        // theme={{calendarBackground: 'red', agendaKnobColor: 'green'}}
+        // renderDay={this.renderDay}
+        // hideExtraDays={false}
+        // showOnlySelectedDayItems
+        // reservationsKeyExtractor={this.reservationsKeyExtractor}
+      />
+    );
+  }
+
+  loadItems = (day: DateData) => {
+    const items = this.state.items || {};
+
+    setTimeout(() => {
+      for (let i = -15; i < 85; i++) {
+        const time = day.timestamp + i * 24 * 60 * 60 * 1000;
+        const strTime = this.timeToString(time);
+
+        if (!items[strTime]) {
+          items[strTime] = [];
+
+          const numItems = Math.floor(Math.random() * 3 + 1);
+          for (let j = 0; j < numItems; j++) {
+            items[strTime].push({
+              name: "Item for " + strTime + " #" + j,
+              height: Math.max(50, Math.floor(Math.random() * 150)),
+              day: strTime,
+            });
+          }
+        }
+      }
+
+      const newItems: AgendaSchedule = {};
+      Object.keys(items).forEach((key) => {
+        newItems[key] = items[key];
+      });
+      this.setState({
+        items: newItems,
+      });
+    }, 1000);
+  };
+
+  renderDay = (day) => {
+    if (day) {
+      return <Text style={styles.customDay}>{day.getDay()}</Text>;
+    }
+    return <View style={styles.dayItem} />;
+  };
+
+  renderItem = (reservation: AgendaEntry, isFirst: boolean) => {
+    const fontSize = isFirst ? 16 : 14;
+    const color = isFirst ? "black" : "#43515c";
+
+    return (
+      <TouchableOpacity
+        testID={testIDs.agenda.ITEM}
+        style={[styles.item, { height: reservation.height }]}
+        onPress={() => Alert.alert(reservation.name)}
+      >
+        <Text style={{ fontSize, color }}>{reservation.name}</Text>
+      </TouchableOpacity>
+    );
+  };
+
+  renderEmptyDate = () => {
+    return (
+      <View style={styles.emptyDate}>
+        <Text>This is empty date!</Text>
+      </View>
+    );
+  };
+
+  rowHasChanged = (r1: AgendaEntry, r2: AgendaEntry) => {
+    return r1.name !== r2.name;
+  };
+
+  timeToString(time: number) {
+    const date = new Date(time);
+    return date.toISOString().split("T")[0];
+  }
 }
 
 const styles = StyleSheet.create({
+  item: {
+    backgroundColor: "white",
+    flex: 1,
+    borderRadius: 5,
+    padding: 10,
+    marginRight: 10,
+    marginTop: 17,
+  },
+  emptyDate: {
+    height: 15,
+    flex: 1,
+    paddingTop: 30,
+  },
+  customDay: {
+    margin: 10,
+    fontSize: 24,
+    color: "green",
+  },
+  dayItem: {
+    marginLeft: 34,
+  },
+});
+
+/* const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
@@ -43,4 +194,4 @@ const styles = StyleSheet.create({
     height: 1,
     width: "80%",
   },
-});
+}); */
