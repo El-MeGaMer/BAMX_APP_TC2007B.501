e90868b768c4c02c51df3c026b8d248ac6ef3896
mctuliomontoya <mctulio.2@gmail.com>
2023-11-10 18:14:31 -0700

    fix: specify prop type, any => never

--- Frontend/components/SelectLogButton.tsx
+++ Frontend/components/SelectLogButton.tsx
@@ -1,32 +1,37 @@
-import { Text, View, TouchableOpacity, Pressable } from 'react-native'
-import React, { Component } from 'react'
+import { Text, View, TouchableOpacity, Pressable } from "react-native";
+import React, { Component } from "react";
 
-import { styled  } from "nativewind";
+import { styled } from "nativewind";
 
 const StyledView = styled(View);
 const StyledTouchableOpacity = styled(TouchableOpacity);
 const StyledText = styled(Text);
 const StyledPressable = styled(Pressable);
 
-import { useNavigation } from '@react-navigation/native';
+import { useNavigation } from "@react-navigation/native";
 
-const SelectLogButton = ( {text, desiredLog}) => {  
-    const navigation = useNavigation();
+const SelectLogButton = ({ text, destinatedLog }) => {
+  const navigation = useNavigation();
 
-    // 
-    const showLog = () => {
-      navigation.removeListener
-      navigation.navigate('DisplayLogs', { desiredLog: desiredLog })
-    }
+  //
+  const showLog = () => {
+    navigation.removeListener;
+    navigation.navigate({
+      name: "DisplayLogs",
+      params: { desiredLog: destinatedLog },
+    } as never);
+  };
 
-    return (
-        <StyledPressable
-          onPress={showLog}
-          className=" bg-orange p-3 rounded-xl shadow px-8 pt-4 pb-4 mb-4 w-full max-w-xs items-center focus:bg-red-50"
-        >
-          <StyledText className="text-white font-bold text-md">{text? text: 'asdsd'}</StyledText>
-        </StyledPressable>
-    )
-}
+  return (
+    <StyledTouchableOpacity
+      onPress={showLog}
+      className=" bg-orange p-3 rounded-xl shadow px-8 pt-4 pb-4 mb-4 w-full max-w-xs items-center focus:bg-red-50"
+    >
+      <StyledText className="text-white font-bold text-md">
+        {text ? text : "asdsd"}
+      </StyledText>
+    </StyledTouchableOpacity>
+  );
+};
 
-export default SelectLogButton
+export default SelectLogButton;
