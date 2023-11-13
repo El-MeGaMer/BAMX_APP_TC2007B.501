e1cff1fee0d6d8f71f593cf1707f29d3b52a42da
Dany <daniela.ruizgar1@gmail.com>
2023-11-13 15:10:40 -0700

    add: translated HomePage to spanish

--- Frontend/screens/HomeScreen.tsx
+++ Frontend/screens/HomeScreen.tsx
@@ -41,6 +41,29 @@ import testIDs from "../test/testIDs";
   );
 } */
 
+LocaleConfig.locales['es'] = {
+  monthNames: [
+    'Enero',
+    'Febrero',
+    'Marzo',
+    'Abril',
+    'Mayo',
+    'Junio',
+    'Julio',
+    'Agosto',
+    'Septiembre',
+    'Octubre',
+    'Noviembre',
+    'Diciembre'
+  ],
+  monthNamesShort: ['Ene.', 'Feb.', 'Mar.', 'Abr.', 'May.', 'Jun.', 'Jul.', 'Ago.', 'Sep.', 'Oct.', 'Nov.', 'Dic.'],
+  dayNames: ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'],
+  dayNamesShort: ['Dom.', 'Lun.', 'Mar.', 'Mié.', 'Jue.', 'Vie.', 'Sáb.'],
+  today: "Hoy"
+};
+
+LocaleConfig.defaultLocale = 'es';
+
 interface State {
   items?: AgendaSchedule;
 }
