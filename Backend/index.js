d7cd79f10a8abe55924ef3d177ff39b2e8f5e5f8
GreatUsername63 <RamirezSantiago244@gmail.com>
2023-11-13 14:08:30 -0700

    Cambio de direccion y nombre de archivos

--- Backend/index.js
+++ Backend/index.js
@@ -1,7 +1,7 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import router from "./routes/index.js"
-import initScheduledJobs from "./middlewares/scheduledFunctions.js"
+import initScheduledJobs from "./controllers/scheduledActions/cronjobs.js"
 
 const app = express()
 const PORT = 3000
