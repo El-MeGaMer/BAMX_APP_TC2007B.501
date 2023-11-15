8bd5d1c829bed4021e7e601af74b635ec5fecc57
mazomenos31 <gustbetancourt@hotmail.com>
2023-11-14 18:46:18 -0700

    removi funciones no usadas en controladores

--- Backend/controllers/Bitacora/alimento_compartido_controller.js
+++ Backend/controllers/Bitacora/alimento_compartido_controller.js
@@ -3,69 +3,6 @@ import { PrismaClient } from "@prisma/client"
 const prisma = new PrismaClient
 
 //-----------------------------------------------------
-//CRUD Basico
-
-//Crear bitacora de Alimento Compartido
-export const postAlimentoCompartido = async (req, res) => {
-    const { idUsuarioEmisor, idArea, nombre, capacidad,
-        manometro, estadoFisico, mangueras, seguro, etiquetas, holograma,
-        ultimaRevision, proximaRecarga, observaciones } = req.body
-    try {
-        
-        console.log(req.body)
-      
-          const newRecordatorio = await prisma.recordatorios.create({
-            data: {
-              nombre: "bitacora",
-              descripcion: "Bitacora de Extintores Creada",
-              horaInicial: "2023-11-11T12:30:45.123Z",
-              horaFinal: "2023-11-11T12:30:45.123Z"
-            },
-          });
-
-        const result = await prisma.bitacoraExtintores.create({
-            data: {
-                idUsuarioEmisor,
-                idArea,
-                idRecordatorio: newRecordatorio.id,
-                nombre, capacidad,
-                manometro, estadoFisico, mangueras, seguro, etiquetas, holograma,
-                ultimaRevision, proximaRecarga, observaciones, 
-                estado: "Created"
-            }
-        })
-
-        res.json(result)
-
-        
-    } catch (error) {
-        if (process.env.NODE_ENV !== 'test') {
-            console.log('Error! Could not add the entry:', error)
-        }
-    }
-}
-
-//Actualizar bitacora de Alimento Compartido
-export const updateAlimentoCompartido = async (req, res) => {
-
-}
-
-//obtener todas las bitacoras de Alimento Compartido
-export const getAllAlimentoCompartido = async (req, res) => {
-
-}
-
-//obtener bitacora de Alimento Compartido por su ID
-export const getAlimentoCompartidoByID = async (req, res) => {
-
-}
-
-//borrar entrada de bitacora de Alimento Compartido
-export const delAlimentoCompartido = async (req, res) => {
-
-}
-
-//-----------------------------------------------------
 
 //Funciones Especificas
 
--- Backend/controllers/Bitacora/cribaFV_controller.js
+++ Backend/controllers/Bitacora/cribaFV_controller.js
@@ -2,68 +2,6 @@ import { PrismaClient } from "@prisma/client"
 
 const prisma = new PrismaClient
 
-//-----------------------------------------------------
-//CRUD Basico
-
-//Crear bitacora de criba frutas y verduras
-export const postCribaFV = async (req, res) => {
-    const { idUsuarioEmisor, idArea, nombre, capacidad,
-        manometro, estadoFisico, mangueras, seguro, etiquetas, holograma,
-        ultimaRevision, proximaRecarga, observaciones } = req.body
-    try {
-        
-        console.log(req.body)
-      
-          const newRecordatorio = await prisma.recordatorios.create({
-            data: {
-              nombre: "bitacora",
-              descripcion: "Bitacora de Extintores Creada",
-              horaInicial: "2023-11-11T12:30:45.123Z",
-              horaFinal: "2023-11-11T12:30:45.123Z"
-            },
-          });
-
-        const result = await prisma.bitacoraExtintores.create({
-            data: {
-                idUsuarioEmisor,
-                idArea,
-                idRecordatorio: newRecordatorio.id,
-                nombre, capacidad,
-                manometro, estadoFisico, mangueras, seguro, etiquetas, holograma,
-                ultimaRevision, proximaRecarga, observaciones, 
-                estado: "Created"
-            }
-        })
-
-        res.json(result)
-
-        
-    } catch (error) {
-        if (process.env.NODE_ENV !== 'test') {
-            console.log('Error! Could not add the entry:', error)
-        }
-    }
-}
-
-//Actualizar bitacora de criba frutas y verduras
-export const updateCribaFV = async (req, res) => {
-
-}
-
-//obtener todas las bitacoras de criba frutas y verduras
-export const getAllCribaFV = async (req, res) => {
-
-}
-
-//obtener bitacora de criba frutas y verduras por su ID
-export const getCribaFVByID = async (req, res) => {
-
-}
-
-//borrar entrada de bitacora de criba frutas y verduras
-export const delCribaFV = async (req, res) => {
-
-}
 
 //-----------------------------------------------------
 
