247df82724a3df55d27f45450019a28ecda9a11e
Andre Castillo <a01254180@itesm.mx>
2023-11-14 18:01:52 -0700

    Added more validations for recordatorio_test

--- Backend/test/recordatorio_test.js
+++ Backend/test/recordatorio_test.js
@@ -12,8 +12,8 @@ describe('Recordatorio Controllers', () => {
         const body = {
             nombre: "Recordatorio Prueba",
             descripcion: "Este recordatorio es de prueba",
-            horaInicial: "2024-12-13T06:00:00Z",
-            horaFinal: "2024-12-13T16:11:11Z"
+            horaInicial: "2024-12-13T06:00:00.000Z",
+            horaFinal: "2024-12-13T16:11:11.000Z"
         }
 
         chai.request(app)
@@ -22,6 +22,10 @@ describe('Recordatorio Controllers', () => {
         .end((err, res) => {
             idCreated = res.body.id
             expect(res).to.have.status(200);
+            expect(res.body.nombre).to.deep.equal("Recordatorio Prueba");
+            expect(res.body.descripcion).to.deep.equal("Este recordatorio es de prueba");
+            expect(res.body.horaInicial).to.deep.equal("2024-12-13T06:00:00.000Z");
+            expect(res.body.horaFinal).to.deep.equal("2024-12-13T16:11:11.000Z");
         });
     })
 
@@ -29,22 +33,33 @@ describe('Recordatorio Controllers', () => {
         chai.request(app)
         .get('/recordatorio/')
         .end((err, res) => {
+            // Obtener las IDs de la respuesta
+            let IDs = [];
+            res.body.forEach(element => {
+                IDs.push(element.id);
+            });
             expect(res).to.have.status(200);
+            expect(IDs).to.deep.include(idCreated);
         });
     })
 
     it('Actualizar recordatorio', async () => {
         const body = {
-            nombre: "Recordatorio actualizado",
+            nombre: "Nuevo Recordatorio",
             descripcion: "Este recordatorio se ha actualizado",
-            horaInicial: "2025-12-13T06:00:00Z",
-            horaFinal: "2024-12-13T06:00:00Z"
+            horaInicial: "2025-12-13T06:00:00.000Z",
+            horaFinal: "2025-12-13T16:11:11.000Z"
         }
         chai.request(app)
         .put(`/recordatorio/${idCreated}`)
         .send(body)
         .end((err, res) => {
             expect(res).to.have.status(200);
+            expect(res.body.id).to.equal(idCreated);
+            expect(res.body.nombre).to.deep.equal("Nuevo Recordatorio");
+            expect(res.body.descripcion).to.deep.equal("Este recordatorio se ha actualizado");
+            expect(res.body.horaInicial).to.deep.equal("2025-12-13T06:00:00.000Z");
+            expect(res.body.horaFinal).to.deep.equal("2025-12-13T16:11:11.000Z");
         });
     })
 
@@ -53,6 +68,7 @@ describe('Recordatorio Controllers', () => {
         .delete(`/recordatorio/${idCreated}`)
         .end((err, res) => {
             expect(res).to.have.status(200);
+            expect(res.body.id).to.equal(idCreated);
         });
     })
 })
