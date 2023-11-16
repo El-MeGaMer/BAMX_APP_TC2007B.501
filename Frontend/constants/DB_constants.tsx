ff87bc4aebbdd4fac1cf1ef35e5913d9b451f032
ximenalopezm <ximenaooflopez@gmail.com>
2023-11-16 00:11:59 -0700

    FIx: Filter syntaxis

--- Frontend/constants/DB_constants.tsx
+++ Frontend/constants/DB_constants.tsx
@@ -1,9 +1,12 @@
 export const DB_FILTERS = {
-    "RECIBO" : "recibo",
-    "CRIBAFV" : "cribafv",
-    "EMPAQUE" : "empaque",
-    "ENTREGA" : "entrega",
-    "ALMACENCOMP" : "almacencomp",
-    "ALMACEN" : "almacen",
-    "CUARTOSFRIOS" : "cuartosfrios"
+    "AREA1" : "recibo",
+    "AREA2" : "cribafv",
+    "AREA3" : "empaque",
+    "AREA4" : "entrega",
+    "AREA5" : "almacencomp",
+    "AREA6" : "almacen",
+    "AREA7" : "cuartosfrios",
+    "INCIDENTE1" : "opcion1",
+    "INCIDENTE2" : "opcion2",
+    "INCIDENTE3" : "opcion3"
 }
