ab5109b2d8ed6a2cc78df48c7bb4b0a7714a877f
ximenalopezm <ximenaooflopez@gmail.com>
2023-11-15 20:22:00 -0700

    Feat: Constant values filter

---
+++ Frontend/constants/DB_constants.tsx
@@ -0,0 +1,9 @@
+export const DB_FILTERS = {
+    "RECIBO" : "recibo",
+    "CRIBAFV" : "cribafv",
+    "EMPAQUE" : "empaque",
+    "ENTREGA" : "entrega",
+    "ALMACENCOMP" : "almacencomp",
+    "ALMACEN" : "almacen",
+    "CUARTOSFRIOS" : "cuartosfrios"
+}
