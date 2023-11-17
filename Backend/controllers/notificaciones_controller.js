29fa29790d1756245f00f5d9ce94dc1fa065bf70
GreatUsername63 <RamirezSantiago244@gmail.com>
2023-11-16 18:03:42 -0700

    Correos de notificaciones

--- Backend/controllers/notificaciones_controller.js
+++ Backend/controllers/notificaciones_controller.js
@@ -28,3 +28,36 @@ export async function getNotificacionesUsuario(req, res){
         }
     }
 }
+
+//Fetch de las notificaciones para el mail
+export async function fetchNotifications(startTime, endTime) {
+    try {
+        const notifications = await prisma.notificaciones.findMany({
+            where: {
+                fechaHora: {
+                    lte: endTime,
+                    gte: startTime,
+                }
+            }
+            ,select: {
+                titulo: true,
+                descripcion: true,
+                usuarios: {
+                    select: {
+                        usuario: {
+                            select:
+                            {
+                                correo: true,
+                            }
+                        }
+                    }
+                }
+            }
+        });
+    return notifications;
+    } catch (error) {
+        if (process.env.NODE_ENV !== 'test') {
+            console.log('Error! Could not add the entry:', error)
+        }
+    }
+  }
--- Backend/controllers/scheduledActions/cronjobs.js
+++ Backend/controllers/scheduledActions/cronjobs.js
@@ -1,5 +1,7 @@
 import cron from 'node-cron'
 import { postBitacorasMadrugada } from './crearBitacoras.js'
+import { fetchNotifications } from '../notificaciones_controller.js'
+import { sendNotifMail } from '../mails_controller.js'
 
 function initScheduledJobs(){
     /*const cronJobExample = cron.schedule("52 16 * * *", () => {
@@ -10,12 +12,31 @@ function initScheduledJobs(){
     //Sec,Min,Hora,Dia,Mes,Dia de la semana
     //Doc: https://www.npmjs.com/package/node-cron
     const horaCrearBitacorasMadrugada = "0 4 * * *"
+    const horaEscanearPorNotifs = "* * * * *"
 
     const crearBitacorasMadrugada = cron.schedule(horaCrearBitacorasMadrugada,postBitacorasMadrugada)
+    
+    //EscanearPorNotifs para el mail
+    const EscanearPorNotifs = cron.schedule(horaEscanearPorNotifs, async () => {
+        const currentTime = new Date();
+        const lastMinute = new Date(currentTime);
+        lastMinute.setMinutes(currentTime.getMinutes() - 10000000);
+      
+        const notifications = await fetchNotifications(lastMinute, currentTime);
+      
+        //console.log('Notifications found:', notifications);
+        
+        for(const i in notifications){
+            console.log("aaaa")
+            sendNotifMail(notifications[i])
+        }
+      })
+
 
     //Inicia que se cuenten todos estos cron jobs
     //cronJobExample.start()
     crearBitacorasMadrugada.start()
+    EscanearPorNotifs.start()
 }
 
 export default initScheduledJobs
