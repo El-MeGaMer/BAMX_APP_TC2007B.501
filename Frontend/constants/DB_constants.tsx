6f292398e2482a002b631063c50c6322fc951e40
ximenalopezm <ximenaooflopez@gmail.com>
2023-11-18 23:32:01 -0700

    Delete: not used options

--- Frontend/constants/DB_constants.tsx
+++ Frontend/constants/DB_constants.tsx
@@ -6,7 +6,4 @@ export const DB_FILTERS = {
     "AREA5" : "almacencomp",
     "AREA6" : "almacen",
     "AREA7" : "cuartosfrios",
-    "INCIDENTE1" : "opcion1",
-    "INCIDENTE2" : "opcion2",
-    "INCIDENTE3" : "opcion3"
 }
