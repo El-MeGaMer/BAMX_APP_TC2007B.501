983a83b417e310a85844a082b90f64bdb005cebf
mctuliomontoya <mctulio.2@gmail.com>
2023-11-21 19:10:19 -0700

    feat: modify to add conditional rendering

--- Frontend/components/SelectLogButton.tsx
+++ Frontend/components/SelectLogButton.tsx
@@ -3,10 +3,15 @@ import React, { Component } from "react";
 
 import { styled } from "nativewind";
 
+import { TableInitialValues } from "../constants/TableInitialValues";
+
 const StyledView = styled(View);
 const StyledTouchableOpacity = styled(TouchableOpacity);
 const StyledText = styled(Text);
 const StyledPressable = styled(Pressable);
+
+
+
 
 import { useNavigation } from "@react-navigation/native";
 
@@ -16,16 +21,17 @@ const SelectLogButton = ({ text, destinatedLog }) => {
   //
   const showLog = () => {
     navigation.removeListener;
+    console.log(TableInitialValues[destinatedLog])
     navigation.navigate({
       name: "DisplayLogs",
-      params: { desiredLog: destinatedLog },
+      params: { desiredLog: destinatedLog, initialValues: TableInitialValues[destinatedLog], logTitle: text},
     } as never);
   };
 
   return (
     <StyledTouchableOpacity
       onPress={showLog}
-      className=" bg-orange p-3 rounded-xl shadow px-8 pt-4 pb-4 mb-4 w-full max-w-xs items-center focus:bg-red-50"
+      className=" bg-orange p-3 rounded-xl shadow px-8 pt-4 pb-4 mb-4 w-4/5 items-center focus:bg-red-50"
     >
       <StyledText className="text-white font-bold text-md">
         {text ? text : "asdsd"}
