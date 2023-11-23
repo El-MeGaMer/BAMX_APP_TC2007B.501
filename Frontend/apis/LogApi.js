7cf04591091791cadba4e292180aaa1141b555e9
Jaza2401 <alex.jauregui.zarate@gmail.com>
2023-11-22 17:21:30 -0700

    Created APIs for Empaque, Almacen, Recibo and Incidente logs

--- Frontend/apis/LogApi.js
+++ Frontend/apis/LogApi.js
@@ -1,9 +1,36 @@
 
 const URL = "http://localhost:3000"
 
-// Function to export 
-export async function LogEmpaque(idUser, idLog, body){
-    const url = URL + '/bitacoras/Empaque/' + idLog + "/" + idUser;
+// Create Log for "Incidente" ---------------------------------------
+
+export async function CreateIncidente(area, desc, img){
+
+    const url = URL + "/bitacoras/Incidente/create";
+
+    const formData = new FormData();
+    formData.append('area', area);
+    formData.append('descripcion', desc);
+    formData.append('photo', img);
+
+    const options = {
+        method: "POST",
+        body: formData
+    }
+
+    const rawResponse = await fetch(url, options);
+    const response = await rawResponse.json();
+}
+
+// Fill Logs APIs ----------------------------------------------------
+
+// Function to fill Logs for the "Empaque" area
+
+export async function UpdateLogEmpaque(idUser, idLog, body){
+
+    const userId = String(idUser); 
+    const logId = String(idLog);
+
+    const url = URL + '/bitacoras/Empaque/' + logId + "/" + userId;
     const options = {
         method: "PUT",
         headers: {
@@ -13,5 +40,46 @@ export async function LogEmpaque(idUser, idLog, body){
     }
     const rawResponse = await fetch(url, options)
     const response = await rawResponse.json();
-    //console.log(JSON.stringify(response))
+    return response;
 }
+
+// Function to fill Logs for the "Almacen" area
+
+export async function UpdateLogAlmacen(idUser, idLog, body){
+
+    const userId = String(idUser); 
+    const logId = String(idLog);
+
+    const url = URL + '/bitacoras/Empaque/' + logId + "/" + userId;
+    const options = {
+        method: "PUT",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(body)
+    }
+    const rawResponse = await fetch(url, options)
+    const response = await rawResponse.json();
+    return response;
+}
+
+// Function to fill Logs for the "Recibo" area
+
+export async function UpdateLogRecibo(idUser, idLog, body){
+
+    const userId = String(idUser); 
+    const logId = String(idLog);
+
+    const url = URL + '/bitacoras/Empaque/' + logId + "/" + userId;
+    const options = {
+        method: "PUT",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(body)
+    }
+    const rawResponse = await fetch(url, options)
+    const response = await rawResponse.json();
+    return response;
+}
+
