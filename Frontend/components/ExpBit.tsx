e84c833f4e1d1023333e20049e51cc8b26ade44e
NovoxMx <A00835132@tec.mx>
2023-11-22 19:30:27 -0700

    si

--- Frontend/components/ExpBit.tsx
+++ Frontend/components/ExpBit.tsx
@@ -5,28 +5,33 @@ import { shareAsync } from 'expo-sharing';
 import * as FileSystem from 'expo-file-system';
 
 export default function ExpBit() {
-  const [userData, setUserData] = useState(null);
+  const [userData, setUserData] = useState([]);
   const [columnNames, setColumnNames] = useState([]);
-  const [fileName, setFileName] = useState('defaultFileName.pdf'); // Nombre predeterminado
-
+  const [fileName, setFileName] = useState('defaultFileName.pdf');
   useEffect(() => {
     fetchUserData();
   }, []);
 
   const fetchUserData = async () => {
     try {
-      const response = await fetch('https://jsonplaceholder.typicode.com/todos/');
+      const response = await fetch('http://10.41.34.161:3000/bitacoras/export');
       const data = await response.json();
-
-      // Obtener nombres de columnas
-      const columns = data.length > 0 ? Object.keys(data[0]) : [];
+      console.log(data);
+  
+      const userData = Object.values(data)?.[0]?.[47];
+  
+      const columns = Object.keys(userData);
+  
       setColumnNames(columns);
-
-      setUserData(data);
+      setUserData([userData]);
+      console.log(userData);
+      console.log(columns);
     } catch (error) {
       console.error('Error al obtener datos:', error);
     }
   };
+  
+  
 
   const generatePdf = async () => {
     try {
@@ -34,8 +39,6 @@ export default function ExpBit() {
         html: generateHtml(),
         base64: true,
       });
-
-      // Cambia el nombre del archivo antes de compartirlo
       const newFileUri = `${file.uri.substring(0, file.uri.lastIndexOf('/') + 1)}${fileName}`;
       await FileSystem.moveAsync({
         from: file.uri,
@@ -49,48 +52,18 @@ export default function ExpBit() {
   };
 
   const generateHtml = () => {
-    if (!userData || userData.length === 0 || columnNames.length === 0) {
-      return '';
-    }
-
+    
+  
     const htmlContent = `
-      <html>
-        <head>
-          <style>
-            table {
-              width: 100%;
-              border-collapse: collapse;
-              margin-bottom: 20px;
-            }
-            table, th, td {
-              border: 1px solid black;
-            }
-            th, td {
-              padding: 8px;
-              text-align: left;
-            }
-            th {
-              background-color: #f2f2f2;
-            }
-          </style>
-        </head>
-        <body>
-          <table>
-            <tr>
-              ${columnNames.map((columnName, index) => `<th key=${index}>${columnName}</th>`).join('')}
-            </tr>
-            ${userData.map((data, index) => (
-              `<tr key=${index}>
-                ${columnNames.map(columnName => `<td key=${columnName}>${data[columnName]}</td>`).join('')}
-              </tr>`
-            )).join('')}
-          </table>
-        </body>
-      </html>
-    `;
 
+    `;
+  
     return htmlContent;
   };
+  
+  
+  
+
 
   return (
     <View style={styles.container}>
