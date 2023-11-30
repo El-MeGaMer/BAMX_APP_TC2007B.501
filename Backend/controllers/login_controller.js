67f943bc6287556ecf2d44b12ea48dac7a961362
m <m@tag.local>
2023-11-29 19:08:29 -0700

    changed email for sending otps

--- Backend/controllers/login_controller.js
+++ Backend/controllers/login_controller.js
@@ -65,22 +65,21 @@ export const genOTP =  async (req, res) => {
     // Send email
 
 	// put your ip (with expo port) here if you wish to test. for installed apps maybe 127.0.0.1? or expo link if published
-	const expoIP = "";
+	const expoIP = "192.168.1.130:8081";
     const emailMessage = `<a href='exp://${expoIP}/?otp=${OTP}&email=${req.body.email}'> Click para iniciar sesion </a>` ;
 
-	console.log(emailMessage);
     const transporter = nodemailer.createTransport({
-		host: "smtp.gmail.com",
+		host: "mail.bahermosillo.org.mx",
 		port: 465,
 		secure: true,
 		auth: {
-			user: 'banmxprueba@gmail.com',
-			pass: process.env.MAIL_PASSWORD
+			user: 'bahermos_system@bahermosillo.org.mx',
+			pass: 'bahermos_system' 
 		}
     });
 
     const mailOptions = {
-		from: "BAMX <lider_area_test_bamx@bahermosillo.org.mx>",
+		from: "BAMX <bahermos_system@bahermosillo.org.mx>",
         to: req.body.email,
         subject: "OTP",
         html: emailMessage 
@@ -88,6 +87,7 @@ export const genOTP =  async (req, res) => {
 
     transporter.sendMail(mailOptions, (error) => {
         if (error) {
+			console.log(error);
 			res.status(400).json({error: "Error enviando correo. Intente de nuevo."});
         } else {
             res.status(200).json({message: "Success"});
