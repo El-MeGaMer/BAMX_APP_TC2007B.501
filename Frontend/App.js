35f90344c4b14323acbee884ce3ef149e8c1235b
Andre Castillo <a01254180@itesm.mx>
2023-12-03 21:17:47 -0700

    Login screen added with correct functionality

--- Frontend/App.js
+++ Frontend/App.js
@@ -8,17 +8,22 @@ import Navigation from "./navigation";
 import { useColorScheme } from "react-native";
 
 import Login from "./screens/Login";
+import { useState } from "react";
 
 export default function App() {
   const isLoadingComplete = useLoadedAssets();
   const colorScheme = useColorScheme();
+  const [LoggedIn, setLoggedIn] = useState(false)
+  const [Role, setRole] = useState("")
+  const [Id, setId] = useState(0)
 
   if (!isLoadingComplete) {
     return null;
   } else {
     return (
       <SafeAreaProvider>
-        <Navigation colorScheme={colorScheme} />
+        {LoggedIn && Id != 0 && <Navigation colorScheme={colorScheme} />}
+        {!LoggedIn && <Login setLoggedIn={setLoggedIn} setRole={setRole} setId={setId} />}
         <StatusBar />
       </SafeAreaProvider>
     );
